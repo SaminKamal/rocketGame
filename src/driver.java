e4e00c174ab8b2a5f48aa3acb3e75d890e8d5c7d
SaminKamal <95001715+SaminKamal@users.noreply.github.com>
2021-12-04 17:39:24 -0500

    pls look and tell me why this don't work

--- src/driver.java
+++ src/driver.java
@@ -10,36 +10,14 @@ public class driver extends JFrame {
      */
 
     public static void main(String args[]) {
-
         JFrame window = new JFrame("Rocket Game");
         game linus = new game();
-        /*
-        leftRocket = new leftSideRocket();
-        rightRocket = new rightSideRocket();
-        mainRocket = new mainRocket();
-
-         */
-        //JPanel pane = new game();
-       // pane.setBackground(Color.BLACK);
-        //window.add(pane);
 
         window.add(linus);
         window.setPreferredSize(new Dimension(1000, 1000));
         window.setVisible(true);
         window.setDefaultCloseOperation(EXIT_ON_CLOSE);
         window.pack();
-
-
-
-
-
-
-
-        //testing purposes
-
-        //git test hello
-
-        //DOES THIS ALSO WORK
     }
 }
 
