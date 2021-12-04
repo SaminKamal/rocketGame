7b5c2b2024f6c321b47fc0c24b9e81885257d2ca
SaminKamal <95001715+SaminKamal@users.noreply.github.com>
2021-12-04 17:40:01 -0500

    pls look and tell me why this don't work

--- src/driver.java
+++ src/driver.java
@@ -2,13 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 
 public class driver extends JFrame {
-    /*
-    public static rocket leftRocket;
-    public static rocket rightRocket;
-    public static rocket mainRocket;
-
-     */
-
     public static void main(String args[]) {
         JFrame window = new JFrame("Rocket Game");
         game linus = new game();
